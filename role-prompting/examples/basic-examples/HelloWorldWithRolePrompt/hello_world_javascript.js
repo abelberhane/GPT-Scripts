a2d80f7da4520090ae87d4695844ee6e2125b1c9
Abel Berhane <20795692+abelberhane@users.noreply.github.com>
2023-10-11 10:00:42 -0400

    v1 hello_world_javascript.js

---
+++ role-prompting/examples/basic-examples/HelloWorldWithRolePrompt/hello_world_javascript.js
@@ -0,0 +1,10 @@
+// Role-prompt: Write a beginner-friendly "Hello, World!" program in JavaScript, demonstrating best practices for function structure.
+
+function main() {
+    // Encapsulating code in a main function to demonstrate good structuring practices for beginners.
+    const message = 'Hello, World!';  // Using 'const' to define the message variable, teaching the concept of variable immutability.
+    console.log(message);  // Logging the message to the console, a common task in JavaScript applications.
+}
+
+// Calling the main function to execute the program, showing beginners how functions are invoked.
+main();
