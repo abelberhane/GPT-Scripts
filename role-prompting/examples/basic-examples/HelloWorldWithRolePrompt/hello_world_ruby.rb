fa9933a590ff60a6a6b878883a55e2b53e73c2c2
Abel Berhane <20795692+abelberhane@users.noreply.github.com>
2023-10-11 10:00:59 -0400

    v1 hello_world_ruby.rb

---
+++ role-prompting/examples/basic-examples/HelloWorldWithRolePrompt/hello_world_ruby.rb
@@ -0,0 +1,10 @@
+# Role-prompt: Write a "Hello, World!" program in Ruby that adheres to the Ruby style guide and is suitable for beginners.
+
+def main
+  # Encapsulating the main logic in a method to promote good organization practices for beginners.
+  message = 'Hello, World!'  # Using a local variable to store the string, showing variable usage in Ruby.
+  puts message  # Outputting the message to the console.
+end
+
+# Invoking the main method to run the program, helping beginners understand the flow of a Ruby script.
+main()
